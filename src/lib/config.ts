8177d6db64b93848be550e1d904011061402488b
Angelica Willianto <angelicawillianto@gmail.com>
2025-04-06 00:42:25 +0800

    Update config.ts

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -2,3 +2,12 @@ export const routes = {
 	markets: "/",
 	myPositions: "/my-positions",
 };
+
+export const contractAddresses = {
+	baseSepolia: {
+		cnMarket: "0x29F449C65dE4391ca3FCA9180d42F9bC2b6041f8",
+		cnMarketResolver: "0xB1F797afe4E7421724C671257a1BE9877159A168",
+		tweetVerifier: "0x4FE12329176e4cA8D8bC6bE73295d7A42C018720",
+		embeddingVerifier: "0xaF320498B95b1959E2ec4ea6BBC8aeaedb0ea346",
+	},
+};
